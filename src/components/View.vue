bd24da9968157b45d30c895fada81b16f0fb3a26
MaryyamSalah <maryyamslah20@gmail.com>
2023-05-29 14:33:45 +0300

    fixed API

--- src/components/View.vue
+++ src/components/View.vue
@@ -2,26 +2,25 @@
    <div class="container m-20 mt-4 mb-4 p-5 bg-dark text-white rounded">
     <div class="row">
       <div class="col-lg-8">
-       <!-- <img alt="Image 4" class="img-fluid float-start" src="{{products.image}}"> -->
-       <img :alt="product-pic" class="img-fluid float-start mb-4" :src="`${products.image}`" >
+       <img :alt="product-pic" class="img-fluid float-start mb-4 rounded-2" :src="`${products.thumbnail}`" >
       </div>
       <div class="col-lg-4 text-start">
         <div class="row">
           <div class="col-md-6">
             <h6>Category Title</h6>
-            <p class="border bg-info p-2 text-black">{{products.category}}</p>
+            <p class="border bg-info p-2 text-black rounded-2">{{products.category}}</p>
           </div>
           <div class="col-md-6 ">
             <h6>Price($)</h6>
-            <p class="border bg-info p-2 text-black ">{{products.price}}</p>
+            <p class="border bg-info p-2 text-black rounded-2 ">{{products.price}}</p>
           </div>
           <div class="col-md-6">
             <h6>Product Name</h6>
-            <p class="border bg-info p-2 text-black">{{products.title}}</p>
+            <p class="border bg-info p-2 text-black rounded-2">{{products.title}}</p>
           </div>
           <div class="col-md-6">
             <h6>Status</h6>
-            <p class="border bg-info p-2 text-black">{{products.id}}</p>
+            <p class="border bg-info p-2 text-black rounded-2">{{products.id}}</p>
           </div>
 
         </div>
@@ -29,7 +28,7 @@
       </div>
        <div class="col-md-20 text-start">
             <h6 >Description</h6>
-            <p class="border bg-info p-4 text-black">{{products.description}}</p>
+            <p class="border bg-info p-4 text-black rounded-2">{{products.description}}</p>
           </div>
     </div>
   </div>
@@ -54,7 +53,7 @@ mounted() {
 },
 methods: {
   fetchProductDetails() {
-    axios.get(' http://localhost:3000/products/1')
+    axios.get('https://dummyjson.com/products/1')
       .then(response => {
         this.products = response.data;
         this.images = response.data;
